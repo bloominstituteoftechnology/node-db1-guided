b7afeb6bc62b37b1805481b5a336e2799cc45525
James Blacklock <jamesblacklock@protonmail.com>
2022-05-16 14:09:02 -0400

    committing to "lecture"

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const PORT = process.env.PORT || 9000
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`)
 })
+
+// const db = require('./data/db-config');
+// // DELETE FROM customers WHERE customername = 'Die Wandernde Kuh' AND customerid = 86
+// db('customers')
+//   // .insert([
+//   //   { contactname: '1', address: 'a' },
+//   //   { contactname: '2', address: 'b' },
+//   // ])
+//   // .where('customerid', 92)
+//   .then(result => {
+//     console.log(result);
+//     process.exit();
+//   });
+
+
